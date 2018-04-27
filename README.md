a9f425a3ee1c9d9dbe54a6dc92be730ef69af2ac
St Fu <it@s-fuhrmann.de>
2018-04-27 02:13:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# FCCWeather
+
+## todo:
+-CSS
+-User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+-myButton
+
+
+## works:
+-Error Handling
+-User Story: I can push a button to toggle between Fahrenheit and Celsius.
+-User Story: I can see the weather in my current location.
+
+
+## notes:
+{
+    "coord":{"lon":11.54,"lat":48.13},
+    "weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F01n.png?1499366020783"}],
+    "base":"stations",
+    "main":{"temp":7.49,"pressure":1019,"humidity":66,"temp_min":6,"temp_max":9},
+    "visibility":10000,
+    "wind":{"speed":1.71,"deg":100},
+    "clouds":{"all":0},
+    "dt":1524781200,
+    "sys":{"type":1,"id":4887,"message":0.0064,"country":"DE","sunrise":1524715357,"sunset":1524766879},
+    "id":6940463,
+    "name":"Altstadt",
+    "cod":200
+}
+
+°C = (°F − 32) / 1,8
---
+++ data/api.js
@@ -0,0 +1,138 @@
+var latitude;
+var longitud;
+var temp;
+var celsius = true;
+
+$(document).ready(function() {
+  if (!navigator.geolocation){
+    //output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    console.log("Geolocation is not supported by your browser");
+    return;
+  }
+  else{
+      navigator.geolocation.getCurrentPosition(function (position) {
+      latitude  = position.coords.latitude;
+      longitude = position.coords.longitude;
+      console.log(latitude);
+
+      var output = document.getElementById("google");
+      var img = new Image();
+      img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+  
+      output.appendChild(img);
+      myWeather(latitude, longitude);
+      })
+  }
+  console.log("hallo2");
+  //Button:
+  $("#toogle").click(function () {
+    farOrCel(temp);
+    
+  
+  });
+})
+
+function myWeather(latitude, longitude) {
+  console.log("hi");
+  myUrl = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+  console.log(myUrl);
+  $.ajax({
+    headers:{
+    Accept: "application/json"
+    },
+    url: myUrl,
+    success: function(r){
+      console.log(r.main.temp);
+      temp = r.main.temp;
+   
+      },
+      error: function(a, b, c){
+        console.log(a);
+        console.log(b);
+        console.log(c);
+      }
+  })
+   
+}
+ 
+
+
+
+function farOrCel(temp){
+  if(celsius){
+    //gibt fahrenheit
+    console.log("fahren")
+    celsius = false;
+    temp = ((temp*1.8)+32);
+    temp = +temp.toFixed(2)
+    console.log(temp)
+    $("#temp").text(temp);
+    return temp;
+  }
+  //gibt celsuis
+  else {
+    console.log("cel")
+    console.log(temp)
+    celsius = true;
+    $("#temp").text(temp);
+    return temp;
+  }
+
+}
+/* 
+
+
+
+
+function geoLoc() {
+  var output = document.getElementById("out");
+  
+
+ 
+
+
+
+
+    console.log(myUrl);
+    console.log("succ");
+  
+  
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+
+    //behalten IMAGE von Gmaps unten anzuhängen ((width full!))
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  }
+
+  function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+          output.innerHTML = "User denied the request for Geolocation."
+          break;
+      case error.POSITION_UNAVAILABLE:
+          output.innerHTML = "Location information is unavailable."
+          break;
+      case error.TIMEOUT:
+          output.innerHTML = "The request to get user location timed out."
+          break;
+      case error.UNKNOWN_ERROR:
+          output.innerHTML = "An unknown error occurred."
+          break;
+    }
+  }
+  //Async Wartemeldung????
+output.innerHTML = "<p>Locating…</p>";
+
+navigator.geolocation.getCurrentPosition(success, showError);
+console.log("END");
+}
+
+ 
+/*
+     
+
+*/
